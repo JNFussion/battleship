fa5e47abf80f9addb632ea4b739e633e854ffca6
JNFussion <juan.jose.gutierrez.99@gmail.com>
2021-10-25 11:25:58 +0200

    Remove canvas implementation. Add Konva.js, a 2d canvas js library, adapted canvas object to use Konva. Drawn ships snap to grid and keep insde bounds

--- src/canvas.js
+++ src/canvas.js
@@ -1,47 +1,153 @@
+import Konva from "konva";
+
 const canvas = (canvasDom) => {
-  const canvasElement = canvasDom;
-  const context = canvasElement.getContext("2d");
-
-  function drawBoard() {
-    context.setLineDash([5, 5]);
-    const unit = canvasElement.width / 10;
-    for (let x = unit; x <= canvasElement.width; x += unit) {
-      context.moveTo(x, 0);
-      context.lineTo(x, canvasElement.height);
-    }
-
-    for (let x = unit; x < canvasElement.height; x += unit) {
-      context.moveTo(0, x);
-      context.lineTo(canvasElement.width, x);
-    }
-
-    context.strokeStyle = "#FFF";
-    context.stroke();
-  }
-
-  const init = () => {
-    canvasElement.width = canvasElement.clientWidth;
-    canvasElement.height = canvasElement.clientHeight;
-    drawBoard();
+  const element = canvasDom;
+  const width = element.clientWidth;
+  const height = element.clientHeight;
+  const unitX = width / 10;
+  const unitY = height / 10;
+  const stage = new Konva.Stage({
+    container: element.id,
+    width,
+    height,
+  });
+  const layers = {
+    gridLayer: new Konva.Layer(),
+    shipsLayer: new Konva.Layer(),
   };
 
-  function getMousePos(evt) {
-    const rect = canvasElement.getBoundingClientRect();
-    return {
-      x: evt.clientX - rect.left,
-      y: evt.clientY - rect.top,
-    };
+  stage.add(layers.gridLayer);
+  stage.add(layers.shipsLayer);
+
+  function drawBoard() {
+    for (let x = unitX; x < width; x += unitX) {
+      layers.gridLayer.add(
+        new Konva.Line({
+          points: [x, 0, x, height],
+          stroke: "white",
+          strokeWidth: 1,
+          dash: [10, 5],
+        })
+      );
+    }
+    for (let y = unitY; y < height; y += unitY) {
+      layers.gridLayer.add(
+        new Konva.Line({
+          points: [0, y, width, y],
+          stroke: "white",
+          strokeWidth: 1,
+          dash: [10, 5],
+        })
+      );
+    }
   }
 
-  function getCoords(evt) {
-    const mousePos = getMousePos(evt);
-    return [
-      Math.floor(Math.floor(mousePos.y) / (canvasElement.height / 10)),
-      Math.floor(Math.floor(mousePos.x) / (canvasElement.width / 10)),
-    ];
+  function newRectangle({ x, y }, size) {
+    const newGroup = new Konva.Group({
+      x,
+      y,
+      width: unitX * size,
+      height: unitY,
+      id: `ship-${size}-${x}-${y}`,
+    });
+
+    newGroup.add(
+      new Konva.Rect({
+        x,
+        y,
+        width: unitX * size,
+        height: unitY,
+        fill: "purple",
+        stroke: "red",
+        strokeWidth: 1,
+        name: `body`,
+        draggable: true,
+      })
+    );
+    newGroup.add(
+      new Konva.Rect({
+        x,
+        y,
+        width: unitX * size,
+        height: unitY,
+        fill: "red",
+        stroke: "#FAFAFA",
+        strokeWidth: 1,
+        name: `shadow`,
+        visible: false,
+      })
+    );
+    layers.shipsLayer.add(newGroup);
   }
 
-  return { canvasElement, init, getCoords };
+  /*
+  Keep ships inside canvas
+  Source: https://jsbin.com/gakelemuru/edit?js,output
+*/
+
+  function keepInsideBounds(shape) {
+    const box = shape.getClientRect();
+    const absPos = shape.getAbsolutePosition();
+    const offsetX = box.x - absPos.x;
+    const offsetY = box.y - absPos.y;
+
+    const newAbsPos = { ...absPos };
+    if (box.x < 0) {
+      newAbsPos.x = -offsetX;
+    }
+    if (box.y < 0) {
+      newAbsPos.y = -offsetY;
+    }
+    if (box.x + box.width > stage.width()) {
+      newAbsPos.x = stage.width() - box.width - offsetX;
+    }
+    if (box.y + box.height > stage.height()) {
+      newAbsPos.y = stage.height() - box.height - offsetY;
+    }
+    shape.setAbsolutePosition(newAbsPos);
+  }
+
+  function getShadow(rect) {
+    return rect.getParent().find(".shadow")[0];
+  }
+
+  /*
+    Snap to grid of rect
+    Source: https://medium.com/@pierrebleroux/snap-to-grid-with-konvajs-c41eae97c13f
+  */
+
+  // EVENTS
+
+  layers.shipsLayer.on("dragstart", (e) => {
+    const shadowShip = getShadow(e.target);
+
+    shadowShip.show();
+    shadowShip.moveToTop();
+    e.target.moveToTop();
+  });
+
+  layers.shipsLayer.on("dragend", (e) => {
+    const shadowShip = getShadow(e.target);
+    e.target.position({
+      x: Math.round(e.target.x() / unitX) * unitX,
+      y: Math.round(e.target.y() / unitY) * unitY,
+    });
+    stage.batchDraw();
+    shadowShip.hide();
+  });
+
+  layers.shipsLayer.on("dragmove", (e) => {
+    const shadowShip = getShadow(e.target);
+    shadowShip.position({
+      x: Math.round(e.target.x() / unitX) * unitX,
+      y: Math.round(e.target.y() / unitY) * unitY,
+    });
+    keepInsideBounds(e.target);
+    keepInsideBounds(shadowShip);
+    stage.batchDraw();
+  });
+
+  return { element, drawBoard, newRectangle };
 };
 
 export default canvas;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
+import canvas from "./canvas";
 import "./main.css";
+
+const playerCanvas = canvas(document.getElementById("player-board"));
+const enemyCanvas = canvas(document.getElementById("enemy-board"));
+playerCanvas.drawBoard();
+playerCanvas.newRectangle({ x: 0, y: 0 }, 2);
+playerCanvas.newRectangle({ x: 0, y: 45.3 }, 5);
+enemyCanvas.drawBoard();
